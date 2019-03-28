c6a00126dc08d2671e5cf812609bec4f8ddb2312
Alexandru Dumitrescu <my email address used on my git repo>
2019-03-28 15:16:18 +0200

    update documentation

--- R/mod_met.R
+++ R/mod_met.R
@@ -1,8 +1,15 @@
 #' Compute model evaluation metrics
-#' @description Compute model evaluation metrics to asses the agreement between observed data and various reanalysis datasets
-
+#' @description Compute model evaluation metrics to asses the agreement between observed data and various reanalysis datasets.
+#' @param obs Name of the variable in the object resulted from \code{\link{wide_to_long}} representing the referenece data.
+#' @param est Name of the variable in the object resulted from \code{\link{wide_to_long}} the estimates that are analysed.
+#' @param metrics The indicators to be computed. See Details section with de description of which indicator. The spliting data
+#' frame is based on the \code{link[openair]{cutData}} function (\code{link[openair]} package).
+#' @param split Character or vector of characters containing values to divide into groups in order to compute the metrics.
+#' @importFrom openair cutData
+#' @export
 mod_met <- function(obs = "obs", est = "est",
-                    metrics = c("ME", "MAE", "RMSE", "COR")) {
+                   metrics = c("ME", "MAE", "RMSE", "COR"),
+                    split = "default") {
 
 
   ## root mean square error
