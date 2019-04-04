55f170e8232a7dfe555f5e7759e6413419f5b032
alexdum <my email address used on my git repo>
2019-04-04 10:17:44 +0300

    update documentation

--- R/mod_met.R
+++ R/mod_met.R
@@ -1,7 +1,7 @@
 #' Compute model evaluation metrics
 #' @description Compute model evaluation metrics to asses the agreement between observed data and various reanalysis datasets.
 #' @param obs Name of the variable in the object resulted from \code{\link{wide_to_long}} representing the referenece data.
-#' @param est Name of the variable in the object resulted from \code{\link{wide_to_long}} the estimates that are analysed.
+#' @param est Name of the variable in the object resulted from \code{\link{wide_to_long}} with the estimates that are analysed.
 #' @param metrics The indicators to be computed. See Details section with de description of which indicator.
 #' @param split Character or vector of characters containing values to divide into groups in order to compute the metrics.
 #' The data frame subsetting procedure is based on the \code{\link[openair]{cutData}} function (\pkg{openair}).
